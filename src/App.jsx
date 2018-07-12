d50075492f1e4b3e63f6d43d086c886441ba960b
bumsyalao <alaobunmi93@gmail.com>
2018-07-12 21:51:39 +0100

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Auth from './components/Auth';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 
 const App = () => (
+    
     <Switch>
         <Route exact path="/" component={Auth} />
         <Route path="/dashboard" component={Dashboard} />
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'css/style.scss';
-
+const BASE_NAME = process.env.NODE_ENV === "production" ? "/Rewards-portal" : "/";
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_NAME}>
         <App />
     </BrowserRouter>, document.getElementById('app')
 );
